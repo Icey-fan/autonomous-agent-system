92de58e8b4a9c390fd04da2a6a893f0077afcecb
Icey-fan <fanzixuan0513@gmail.com>
2026-05-08 21:48:08 +0800

    Update __init__.py

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,16 +1,18 @@
-"""Autonomous Agent System - 自主智能体系统"""
+"""
+Autonomous Agent System - Fixed Version
+"""
 
 from .base import BaseAgent, AgentRole, TaskNode, AgentContext, TaskStatus
 from .orchestrator import AgentOrchestrator
 from .planner import PlannerAgent
 from .researcher import ResearcherAgent
 from .reasoner import ReasonerAgent
-from .executor import ExecutorAgent
-from .validator import ValidatorAgent
+from .executor import ExecutorAgent, ToolRegistry
+from .validator import ValidatorAgent, ValidationRule
 from .decision import DecisionAgent
 
-__version__ = "1.0.0"
-__author__ = "Your Name"
+__version__ = "2.0.0"
+__author__ = "Icey-fan"
 __all__ = [
     "BaseAgent",
     "AgentRole",
@@ -22,6 +24,8 @@ __all__ = [
     "ResearcherAgent",
     "ReasonerAgent",
     "ExecutorAgent",
+    "ToolRegistry",
     "ValidatorAgent",
+    "ValidationRule",
     "DecisionAgent",
 ]
